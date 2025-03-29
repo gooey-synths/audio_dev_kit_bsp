293f1b9ae8ef606b36e88c226b554dbb39cf7d07
Alex Pratte <alexpratte16@gmail.com>
2025-03-28 21:16:02 -0400

    Changes for testing

--- base/src/adc/adc.cpp
+++ base/src/adc/adc.cpp
@@ -7,6 +7,7 @@ bool OnChipADC::sAdc12Reset = false;
 ///
 /// Constructor
 /// @param adcNum ADC number (1..3)
+/// @note This assumes a rev. V microcontroller.
 /// @note This function will reset the ADC controller. ADC 1 and 2 are tied
 /// together so they will be reset together.
 /// @todo probably clean this into an ADC12 or 3 controller, subclasses?
@@ -15,8 +16,12 @@ OnChipADC::OnChipADC(uint8_t adcNum)
     : mAdcNum(adcNum), mDmaChannel(dma::DmaController::getInstance(1)->claimAvailableChannel()) {
     assert(adcNum > 0 && adcNum < 4);
     assert(mDmaChannel);
-
-    RCC->D3CCIPR |= RCC_D3CCIPR_ADCSEL_1; // Select per_ck as kernel peripheral clock.
+    
+    RCC->D3CCIPR |= RCC_D3CCIPR_ADCSEL_1;
+    // Select per_ck as kernel clock. Currently per_ck is set to be HSI
+    // This only matters if ADCx_CCR CKMODE is 0.
+    // This does mean that bus and conversion clocks are not synchronized.
+    // Change ADCx_CCR CKMODE > 0 if they should be. I don't think they do?
 
     uint8_t dmaReq = 0;
 
@@ -29,7 +34,7 @@ OnChipADC::OnChipADC(uint8_t adcNum)
             RCC->AHB1RSTR &= ~(RCC_AHB1RSTR_ADC12RST);
             ADC12_COMMON->CCR &= ~(ADC_CCR_PRESC_Msk); // clear prescaler bits
             // TODO: make this more dynamic.
-            ADC12_COMMON->CCR |= 0x0B << ADC_CCR_PRESC_Pos; // set 256 prescaler
+            ADC12_COMMON->CCR |= 0x00 << ADC_CCR_PRESC_Pos; // set 1 prescaler
             sAdc12Reset = true;
         }
         dmaReq = 9;
@@ -43,7 +48,7 @@ OnChipADC::OnChipADC(uint8_t adcNum)
             RCC->AHB1RSTR &= ~(RCC_AHB1RSTR_ADC12RST);
             ADC12_COMMON->CCR &= ~(ADC_CCR_PRESC_Msk); // clear prescaler bits
             // TODO: make this more dynamic.
-            ADC12_COMMON->CCR |= 0x0B << ADC_CCR_PRESC_Pos; // set 256 prescaler
+            ADC12_COMMON->CCR |= 0x00 << ADC_CCR_PRESC_Pos; // set 1 prescaler
             sAdc12Reset = true;
         }
         dmaReq = 10;
@@ -56,7 +61,7 @@ OnChipADC::OnChipADC(uint8_t adcNum)
         RCC->AHB1RSTR &= ~(RCC_AHB4RSTR_ADC3RST);
         ADC3_COMMON->CCR &= ~(ADC_CCR_PRESC_Msk); // clear prescaler bits
         // TODO: make this more dynamic.
-        ADC3_COMMON->CCR |= 0x0B << ADC_CCR_PRESC_Pos; // set 256 prescaler
+        ADC3_COMMON->CCR |= 0x00 << ADC_CCR_PRESC_Pos; // set 1 prescaler
         sAdc12Reset = true;
         dmaReq = 115;
         break;
@@ -65,11 +70,12 @@ OnChipADC::OnChipADC(uint8_t adcNum)
     disable();
     calibrate();
 
-    for (uint8_t iConversion = 0; iConversion < sizeof(mConversions) / sizeof(*mConversions); iConversion++) {
-        mConversions[iConversion] = 0;
+    // Zero conversion buffer
+    for (uint16_t& iConversion : mConversions) {
+        iConversion = 0;
     }
 
-    // Configure dma channel
+    // Configure DMA channel
     mDmaChannel->setTransferType(dma::PER2MEM, true);
     mDmaChannel->setDest((void *)mConversions, sizeof(*mConversions), true);
     mDmaChannel->setSource((void *)&mControllerHw->DR, sizeof(uint16_t), 0);
@@ -86,8 +92,8 @@ void OnChipADC::setSequence(uint8_t *sequence, uint8_t len) {
         return;
     }
 
-    mControllerHw->SQR1 &= ~(ADC_SQR1_L_Msk);           // clear sequence length bits
-    mControllerHw->SQR1 |= (len - 1) << ADC_SQR1_L_Pos; // set sequence length bits
+    mControllerHw->SQR1 &= ~(ADC_SQR1_L_Msk);           // Clear sequence length bits
+    mControllerHw->SQR1 |= (len - 1) << ADC_SQR1_L_Pos; // Set sequence length bits
 
     const uint8_t offsetDelta = (ADC_SQR1_SQ2_Pos - ADC_SQR1_SQ1_Pos);
     uint8_t offsetCounter = ADC_SQR1_SQ1_Pos;
