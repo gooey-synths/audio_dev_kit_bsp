b395f3126493ebd442700871ff75e3b3d25940a2
Alex <alexpratte16@gmail.com>
2025-04-06 13:45:44 -0400

    Exception work I think!

--- base/src/init/main.cpp
+++ base/src/init/main.cpp
@@ -1,7 +1,15 @@
 #include "../tests/tests.hpp"
+#include <malloc.h>
+#include <cstdint>
 
 int main() {
-    test_DAC60508();
+    //uint8_t* a = (uint8_t*)malloc(40);
+    //free(a);
 
+    try {
+        throw "test";
+    } catch( ... ) {
+        ;
+    }
     return 0;
 }
