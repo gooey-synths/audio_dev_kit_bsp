f29d1a0ea24b0ced0d6d3b5279a9e79b2188f334
Alex Pratte <alexpratte16@gmail.com>
2025-08-04 21:38:09 -0400

    Cleanup

--- base/src/timer/basic_timer.cpp
+++ base/src/timer/basic_timer.cpp
@@ -3,46 +3,102 @@
 
 using namespace timer;
 
-
 ///
 /// Constructor.
-/// @param timer_hw Pointer to the timer register struct.
-/// @param timer_num Timer Number
-/// @param freq Target frequency in Hz
+/// @param timerNum Timer number.
+/// @param freq Target frequency in Hz.
+/// @note Timer is disabled at instantiation.
 ///
-BasicTimer::BasicTimer(TIM_TypeDef* timer_hw, uint8_t timer_num, uint32_t freq):
-mTimerHw(timer_hw),
-mTimerNum(timer_num),
+BasicTimer::BasicTimer(uint8_t timerNum, uint32_t freq):
+mTimerNum(timerNum),
 mFreq(freq)
 {
-
-    switch(timer_num){
+    switch(timerNum){
         case 6:
-            RCC->APB1LENR |= 1 << 4; // Enable clock to hw
-            RCC->APB1LRSTR |= 1 << 4; // Set reset bit;
-            RCC->APB1LRSTR &= ~(1 << 4); // Clear reset bit;
+            mTimerHw = TIM6;
+            RCC->APB1LENR |= RCC_APB1LENR_TIM6EN; // Enable clock to hw
+            RCC->APB1LRSTR |= RCC_APB1LRSTR_TIM6RST; // Set reset bit;
+            RCC->APB1LRSTR &= ~(RCC_APB1LRSTR_TIM6RST); // Clear reset bit;
             break;
 
         case 7:
-            RCC->APB1LENR |= 1 << 5; // Enable clock to hw
-            RCC->APB1LRSTR |= 1 << 5; // Set reset bit;
-            RCC->APB1LRSTR &= ~(1 << 5); // Clear reset bit;
+            mTimerHw = TIM7;
+            RCC->APB1LENR |= RCC_APB1LENR_TIM7EN; // Enable clock to hw
+            RCC->APB1LRSTR |= RCC_APB1LRSTR_TIM7RST; // Set reset bit;
+            RCC->APB1LRSTR &= ~(RCC_APB1LRSTR_TIM7RST); // Clear reset bit;
             break;
+
+        default:
+            throw "Invalid timer number";
     }
 
-    // See table 56 in STM RM0433 for explanation.
-    uint8_t d2ppre1 = (RCC->D2CFGR & (7 << 4)) >> 4; // Figure out the APB1 prescaler.
-    uint8_t timpre = (RCC->CFGR & (1 << 15)) >> 15; // Figure out if timpre bit is set.
+    stop();
+}
 
-    uint32_t ker_clk_freq = APB1_TARGET;
-    if(d2ppre1 > 3){
-        ker_clk_freq *= 2;
-        if(timpre && d2ppre1 > 4){
-            ker_clk_freq *= 2;
+///
+/// Get the frequency of the timer kernel clock.
+/// @return The frequency of the timer kernel clock.
+///
+uint32_t BasicTimer::getKerFreq() {
+    // See table 57 in STM RM0433 for explanation.
+    uint8_t d2ppre1 = (RCC->D2CFGR & (RCC_D2CFGR_D2PPRE1_DIV16_Msk)) >> RCC_D2CFGR_D2PPRE1_Pos; // Figure out the APB1 prescaler.
+    bool timpre = !!(RCC->CFGR & (1 << RCC_CFGR_TIMPRE_Pos)); // Figure out if timpre bit is set.
+    
+    uint32_t kerClkFreq = APB1_TARGET;
+
+    if(timpre) {
+        switch (d2ppre1) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+            kerClkFreq *= 1; // Timer clock is equal to bus clock.
+            break;
+        case 4:
+            kerClkFreq *= 2; // Timer clock is 2 times faster than bus clock.
+            break;
+        case 5:
+        case 6:
+        case 7:
+            kerClkFreq *= 4; // Timer clock is 4 times faster than bus clock.
+            break;
+        }
+
+    } else {
+        switch (d2ppre1) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+            kerClkFreq *= 1; // Timer clock is equal to bus clock.
+            break;
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            kerClkFreq *= 2; // Timer clock is equal to bus clock.
+            break;
         }
     }
 
-    mTimerHw->PSC = ker_clk_freq / mFreq;
+    return kerClkFreq;
+}
 
-    mTimerHw->EGR = 1;
+///
+/// Set the frequency of the timer.
+/// @param freq Desired frequency in Hz.
+/// @note timer must be stopped before changing the frequency
+///
+void BasicTimer::setFreq(uint32_t freq) {
+    mTimerHw->PSC = getKerFreq() / mFreq;
+
+    mTimerHw->EGR = TIM_EGR_UG;
+}
+
+///
+/// Get the current freuency of the timer.
+/// @return The current freuency of the timer.
+///
+uint32_t BasicTimer::getFreq() {
+    return getKerFreq() / mTimerHw->PSC
 }
--- base/src/timer/basic_timer.hpp
+++ base/src/timer/basic_timer.hpp
@@ -5,20 +5,20 @@ namespace timer{
 
 class BasicTimer{
 public:
-    BasicTimer(TIM_TypeDef* timer_hw, uint8_t timer_num, uint32_t freq);
+    BasicTimer(uint8_t timerNum, uint32_t freq);
 
     ///
     /// Start the timer.
     ///
     inline void start(){
-        mTimerHw->CR1 |= 1<<0; // Enable the timer.
+        mTimerHw->CR1 |= TIM_CR1_CEN; // Enable the timer.
     }
     
     ///
     /// Stop the timer.
     ///
     inline void stop(){
-        mTimerHw->CR1 &= ~(1<<0); // Disable the timer.
+        mTimerHw->CR1 &= ~(TIM_CR1_CEN); // Disable the timer.
     }
 
     ///
@@ -31,16 +31,21 @@ public:
     ///
     /// Get the current timer count.
     ///
-    inline uint16_t getTime(){
+    inline uint16_t getCount(){
         return mTimerHw->CNT & 0xFFFF;
     }
 
+    void setFreq(uint32_t freq);
+
+    uint32_t getFreq();
+
 private:
-    TIM_TypeDef* mTimerHw;
+
+    static uint32_t getKerFreq();
+
+    TIM_TypeDef* mTimerHw; // Pointer to timer hw
     uint8_t mTimerNum;
     uint32_t mFreq;
-
-
 };
 
 
