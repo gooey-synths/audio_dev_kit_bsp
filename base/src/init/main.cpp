1bb9a1cb1d0b1dbdd1e56ffd05d3b4f36c1f7e35
Alex <alexpratte16@gmail.com>
2025-08-11 20:39:51 -0400

    More build issues

--- base/src/init/main.cpp
+++ base/src/init/main.cpp
@@ -3,11 +3,6 @@
 #include <cstdint>
 
 int main() {
-<<<<<<< HEAD
     test_tinyusb_serial();
-=======
-    test_basic_timer_exceptions();
-    test_basic_timer_accuracy();
->>>>>>> origin/36-add-timer-support
     return 0;
 }
