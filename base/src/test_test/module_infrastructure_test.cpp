cbfc22f8c7535226d0c8344f811698645831f1e8
Alex Pratte <alexpratte16@gmail.com>
2026-05-02 11:53:55 -0400

    ModuleBase, ModuleInterface and silly unit tests

---
+++ base/src/test_test/module_infrastructure_test.cpp
@@ -0,0 +1,56 @@
+#include <modules/module_basics.hpp>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+constexpr size_t numDummyInputs = 6;
+constexpr size_t numDummyOutputs = 7;
+
+class DummyModule: public module_basics::ModuleBase<numDummyInputs, numDummyOutputs> {
+public:
+    uint16_t getInputVal(size_t idx) { return mInputs[idx]; }
+    void setOutputVal(size_t idx, uint16_t val) { mOutputs[idx] = val; }
+
+    MOCK_METHOD(size_t, getInputIdx, (std::string name), (override));
+
+    MOCK_METHOD(size_t, getOutputIdx, (std::string name), (override));
+
+    MOCK_METHOD(void, run, (), (noexcept, override));
+
+    MOCK_METHOD(void, configure, ((std::unordered_map<std::string, std::string>)), (override));
+
+};
+
+// Test setters and getters for ModuleBase
+TEST(ModuleBaseTests, BasicAssertions) {
+    DummyModule dm;
+
+    // Make sure that out of bounds indices return false
+    ASSERT_EQ(dm.inputExists(numDummyInputs+1), false);
+    ASSERT_EQ(dm.outputExists(numDummyOutputs+1), false);
+
+    // Set all of the inputs of the module and make sure they exist
+    for(size_t iInput = 0; iInput < numDummyInputs; iInput++) {
+          ASSERT_EQ(dm.inputExists(iInput), true);
+          dm.setInput(iInput, iInput);
+    }
+    // Make sure that the input we expect is set to the value we expect
+    for(size_t iInput = 0; iInput < numDummyInputs; iInput++) {
+        ASSERT_EQ(dm.getInputVal(iInput), iInput);
+    }
+
+    // Set all of the outputs of the module and make sure they exist
+    for(size_t iOutput = 0; iOutput < numDummyOutputs; iOutput++) {
+          ASSERT_EQ(dm.outputExists(iOutput), true);
+          dm.setInput(iOutput, iOutput);
+    }
+
+    // Make sure that the outputs we expect is set to the value we expect
+    for(size_t iOutput = 0; iOutput < numDummyOutputs; iOutput++) {
+        ASSERT_EQ(dm.getOutput(iOutput), iOutput);
+    }
+}
+
+int main(int argc, char **argv) {
+::testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+}
