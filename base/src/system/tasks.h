e961ec12c662d2ef72e38909a79ec309bba96502
Alex Pratte <alexpratte16@gmail.com>
2026-09-06 10:27:54 -0400

    70 bring in freertos (#72)
    * Added FreeRTOS as a submodule, it builds!
    
    * RTOS is running
    
    * Added blinky task
    
    * Forgot to add FreeRTOS-Kernel as submodule

---
+++ base/src/system/tasks.h
@@ -0,0 +1,12 @@
+#ifndef SYSTEM_TASKS_H
+#define SYSTEM_TASKS_H
+
+// Priorities
+#define MAIN_TASK_PRIO     4
+#define BLINKY_TASK_PRIO   1
+
+// Stack sizes
+#define MAIN_TASK_STACK_SIZE     1024
+#define BLINKY_TASK_STACK_SIZE   256
+
+#endif // SYSTEM_TASKS_H
